8895d1109756fe64b194827840dfe6c734d10a1b
fracture-hikari <1336446085@qq.com>
2025-06-11 15:34:02 +0800

    fix: better rate

--- main.js
+++ main.js
@@ -66,15 +66,15 @@ document.addEventListener('DOMContentLoaded', function () {
                 // For example, highlight if the score is 5 or more.
                 if (sentenceScore > JRRP) { // This is your "bucket" condition
                     processedHTML += `<span class="highlight">${sentence}</span>`;
-                    aiCount += 1;
+                    aiCount += sentence.length;
                 } else {
                     processedHTML += sentence; // Add as plain text
-                    notAiCount += 1;
+                    notAiCount += sentence.length;
                 }
             });
 
             var rate = (aiCount) / (aiCount + notAiCount);
-            aiRate.textContent = `AI率: ${rate}%`
+            aiRate.textContent = `AI率: ${rate * 100}%`
 
             // Set the innerHTML of the display div
             textDisplayDiv.innerHTML = processedHTML;
